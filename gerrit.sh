52bf57e2a733a36cd1e3fdc870a210a76b388cc7
Željko Filipin <zeljko.filipin@gmail.com>
2016-12-16 16:45:57 +0100

    Replacing dashes with underscores in folder names
    It should make it easier to update all repositories.
    
    example: analytics/aggregator/projectview/data becomes
    analytics_aggregator_projectview_data
    
    Also, documentation.

--- gerrit.sh
+++ gerrit.sh
@@ -1,7 +1,19 @@
+# get a list of all gerrit projects
+# https://gerrit.wikimedia.org/r/Documentation/cmd-ls-projects.html
 for p in `ssh -p 29418 zfilipin@gerrit.wikimedia.org gerrit ls-projects`
 do
-  mkdir -p `dirname "$p"`
+  # replacing dashes with underscores in folder names
+  # it should make it easier to update all repositories
+  # http://www.tldp.org/LDP/abs/html/string-manipulation.html#SUBSTRREPL01
+  # ${string//substring/replacement}
+  # example: analytics/aggregator/projectview/data becomes analytics_aggregator_projectview_data
+  d="${p////_}"
+
   echo "----------"
-  echo "git clone" "https://gerrit.wikimedia.org/r/$p" "$p"
-  git clone "https://gerrit.wikimedia.org/r/$p" "$p"
+
+  echo "mkdir -p \`dirname \"$d\"\`"
+  mkdir -p `dirname "$d"`
+
+  echo "git clone \"https://gerrit.wikimedia.org/r/$p\" \"${d}\""
+  git clone "https://gerrit.wikimedia.org/r/$p" "${d}"
 done
