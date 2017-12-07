744c3efff3dbe9e38d854a51d89e5beb26a8d44a
Željko Filipin <zeljko.filipin@gmail.com>
2017-12-07 14:16:09 +0100

    Using find instead of ls to get directories in git-pull
    $ shellcheck *.sh
    
    In git-pull.sh line 9:
    ls -d */ | xargs -n 1 -I % sh -c 'echo ----------; echo %; cd %; git pull'
    ^-- SC2011: Use 'find .. -print0 | xargs -0 ..' or 'find .. -exec .. +' to allow non-alphanumeric filenames.
          ^-- SC2035: Use ./*glob* or -- *glob* so names with dashes won't become options.

--- git-pull.sh
+++ git-pull.sh
@@ -2,8 +2,7 @@
 
 # list all directories, cd into each, run `git pull`
 
-# `ls -d */` lists only directories https://stackoverflow.com/a/3667355/17469
-# `xargs -n 1` splits the list
-# -`I %` replaces `%` with the argument in `sh -c ...`
+# `find` lists only directories
+# -`I %` replaces `%` with the argument in `sh -c`
 
-ls -d */ | xargs -n 1 -I % sh -c 'echo ----------; echo %; cd %; git pull'
+find . -type d -mindepth 1 -maxdepth 1 | xargs -I % sh -c 'echo ----------; echo %; cd %; git pull'
