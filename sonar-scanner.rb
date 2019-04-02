edeba3879c34465027fc1e150a7ead69b5cfd454
Željko Filipin <zeljko.filipin@gmail.com>
2019-04-02 10:40:51 +0200

    Simple progress bar for sonar-scanner

--- sonar-scanner.rb
+++ sonar-scanner.rb
@@ -9,14 +9,16 @@
 # usage: ruby sonar-scanner.rb
 
 folders = Dir.glob('*').select {|f| File.directory? f}
+total = folders.size
+
 # get Sonar API key at https://sonarcloud.io/account/security/
 sonar_api_key = 'not-the-real-one'
 tmp = '/Users/z/tmp/sonar-scanner-cache'
 
-folders.each do |folder|
-  puts '----------'
-  puts folder
-  docker_command = "docker run --volume #{Dir.pwd}/#{folder}/log://var/lib/jenkins/log --volume #{tmp}://cache --volume #{Dir.pwd}/#{folder}://src --env SONAR_API_KEY=#{sonar_api_key} --env ZUUL_PROJECT=#{folder} docker-registry.wikimedia.org/releng/java8-sonar-scanner:0.4.0 -X -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=wmftest -Dsonar.projectKey=#{folder} -Dsonar.projectName=#{folder}"
+folders.each_with_index do |folder, index|
+  puts '-' * 80
+  puts "#{folder} #{index + 1}/#{total} #{(((index + 1) / total.to_f) * 100).round} %"
+  puts '-' * 80
   puts docker_command
   puts `#{docker_command}`
 end
