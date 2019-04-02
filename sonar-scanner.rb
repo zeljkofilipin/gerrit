8cbcc41b2a57e357babb2602dbd8e7109aeedcf8
Željko Filipin <zeljko.filipin@gmail.com>
2019-04-02 10:46:15 +0200

    sonar-scanner: restore docker_command 🤦‍♂️
    Deleted in a previous commit by mistake.

--- sonar-scanner.rb
+++ sonar-scanner.rb
@@ -19,6 +19,7 @@ folders.each_with_index do |folder, index|
   puts '-' * 80
   puts "#{folder} #{index + 1}/#{total} #{(((index + 1) / total.to_f) * 100).round} %"
   puts '-' * 80
+  docker_command = "docker run --volume #{Dir.pwd}/#{folder}/log://var/lib/jenkins/log --volume #{tmp}://cache --volume #{Dir.pwd}/#{folder}://src --env SONAR_API_KEY=#{sonar_api_key} --env ZUUL_PROJECT=#{folder} docker-registry.wikimedia.org/releng/java8-sonar-scanner:0.4.0 -X -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=wmftest -Dsonar.projectKey=#{folder} -Dsonar.projectName=#{folder}"
   puts docker_command
   puts `#{docker_command}`
 end
